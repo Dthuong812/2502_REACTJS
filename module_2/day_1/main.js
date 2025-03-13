54f4bb390cb563ab82f3c0428ccfe97e84249894
Dathuong812 <tranthidathuong812@gmail.com>
2025-03-13 21:09:25 +0700

    loop

---
+++ module_2/day_1/main.js
@@ -0,0 +1,49 @@
+// const a = 5;
+// if(a=>8){
+//     console.log("HSG")
+// }
+// else{
+//     console.log("hsk")
+// }
+// switch (a) {
+//     case 8:
+//         console.log("12345")
+//         break;
+//     case 2:
+//         console.log("123232")
+//         break;
+//     default:
+//         console.log("hehe")
+// }
+const month = 2;
+const year = 2024;
+if (year <= 0) {
+    console.log("Năm không hợp lệ. Vui lòng nhập năm dương.");
+} else {
+    switch (month) {
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+            console.log("31");
+            break;
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            console.log("30");
+            break;
+        case 2:
+            if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) {
+                console.log("29");
+            } else {
+                console.log("28");
+            }
+            break;
+        default:
+            console.log("Tháng không hợp lệ");
+    }
+}
