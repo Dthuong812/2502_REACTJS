057928905c317e5b02fd43a203383d304f3e3136
Thuong <tranthidathuong812@gmail.com>
2025-06-26 21:01:37 +0700

    update addPricesToRecips

--- module_3/day_2/vite-project/src/redux/middleware/recipesMiddleware.js
+++ module_3/day_2/vite-project/src/redux/middleware/recipesMiddleware.js
@@ -1,26 +1,29 @@
 import axios from "axios";
 
+const addPricesToRecipes = (recipes) => {
+  const cachedPrices = JSON.parse(localStorage.getItem("recipePrices")) || {};
+  const updatedPrices = { ...cachedPrices };
+
+  const recipesWithPrice = recipes.map((item) => {
+    if (!updatedPrices[item.id]) {
+      updatedPrices[item.id] = Math.floor(Math.random() * 100) + 50;
+    }
+    return {
+      ...item,
+      price: updatedPrices[item.id],
+    };
+  });
+
+  localStorage.setItem("recipePrices", JSON.stringify(updatedPrices));
+  return recipesWithPrice;
+};
+
 export const fetchRecipes = () => async (dispatch) => {
   dispatch({ type: "FETCH_RECIPES_REQUEST" });
 
   try {
     const response = await axios.get("https://dummyjson.com/recipes");
-
-    const cachedPrices = JSON.parse(localStorage.getItem("recipePrices")) || {};
-    const updatedPrices = { ...cachedPrices };
-
-    const recipesWithPrice = response.data.recipes.map((item) => {
-      if (!updatedPrices[item.id]) {
-        updatedPrices[item.id] = Math.floor(Math.random() * 100) + 50; 
-      }
-      return {
-        ...item,
-        price: updatedPrices[item.id],
-      };
-    });
-
-    localStorage.setItem("recipePrices", JSON.stringify(updatedPrices));
-
+    const recipesWithPrice = addPricesToRecipes(response.data.recipes);
     dispatch({ type: "FETCH_RECIPES_SUCCESS", payload: recipesWithPrice });
   } catch (error) {
     dispatch({ type: "FETCH_RECIPES_FAILURE", payload: error.message });
